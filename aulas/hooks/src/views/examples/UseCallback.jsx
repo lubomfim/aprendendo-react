da585baabbc2aa7a51c03be3259396711636081d
Lucas Bomfim <lucasliko@hotmail.com>
2021-01-31 13:05:11 -0300

    init

--- aulas/hooks/src/views/examples/UseCallback.jsx
+++ aulas/hooks/src/views/examples/UseCallback.jsx
@@ -1,14 +1,9 @@
 import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-import UseCallbackButtons from './UseCallbackButtons'
 
 
 const UseCallback = props => {
-    const [count, setCount] = useState(0)
-    
-    const inc = useCallback(function (delta) {
-        setCount(curr => curr + delta)
-    }, [setCount])
+
 
     return (
         <div className="UseCallback">
@@ -16,10 +11,6 @@ const UseCallback = props => {
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!" />
 
-            <div className="center">
-                <span className="text">{count}</span>
-                <UseCallbackButtons inc={inc} />
-            </div>
         </div>
     )
 }
--- aulas/hooks/src/views/examples/UseCallbackButtons.jsx
+++ aulas/hooks/src/views/examples/UseCallbackButtons.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
 
 const UseCallbackButtons = (props) => {
-    console.log('render....')
     return (
-        <div>
-            <button className="btn" onClick={() => props.inc(6)}>
-                +6
-            </button>
-            <button className="btn" onClick={() => props.inc(12)}>
-                +12
-            </button>
-            <button className="btn" onClick={() => props.inc(18)}>
-                +18
-            </button>
-        </div>
+            <div>aaa</div>
     )
 }
 
